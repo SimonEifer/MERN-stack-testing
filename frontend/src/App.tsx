25b2a91c8ac4cd96f61a7d180a4f8c7fcedfdcde
SimonEifer <simon@eifer.dk>
2025-10-15 17:09:34 +0200

    Seperation of frontend and server folders

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<>
+			<div className="bg-amber-400 text-4xl font-bold">Abetær</div>
+		</>
+	);
+}
+
+export default App;
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-	return (
-		<>
-			<div className="bg-amber-400 text-4xl font-bold">test</div>
-		</>
-	);
-}
-
-export default App;
